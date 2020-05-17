22f8abacf12c446073b85ee2b3376bad876ece31
sumedh105 <sumedh105@gmail.com>
2020-05-17 17:43:16 +0200

    Program to implement level order traversal, non queue version

---
+++ GeeksForGeeks/DataStructures/BinaryTree/Introduction/level_order_traversal_non_queue_version.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct tree
+{
+	int data;
+	struct tree *left_child;
+	struct tree *right_child;
+};
+
+static void insert(struct tree **root_node, int data);
+static int delete(struct tree **root_node);
+static int inorder(struct tree *root_node);
+static int preorder(struct tree *root_node);
+static int postorder(struct tree *root_node);
+static int find_height(struct tree *root_node);
+static int max(int left_height, int right_height);
+static void print_level_order(struct tree *root_node, int height);
+static int print_data(struct tree *root_node, int level);
+
+int main()
+{
+	int choice = 0;
+	int data = 0;
+	int height = 0;
+	struct tree *root_node = NULL;
+
+	while (1)
+	{
+		printf("\nEnter 0 to exit\n");
+		printf("\nEnter 1 to insert a data in the struct tree\n");
+		printf("\nEnter 2 to delete the data from the struct tree\n");
+		printf("\nEnter 3 to display the struct tree in inorder traversal\n");
+		printf("\nEnter 4 to display the struct tree in preorder traversal\n");
+		printf("\nEnter 5 to display the struct tree in postorder traversal\n");
+		printf("\nEnter 6 to find the height of a tree\n");
+		printf("\nEnter 7 to display the tree in level order traversal\n");
+		printf("\nEnter the choice\n");
+		scanf("%d", &choice);
+
+		switch(choice)
+		{
+			case 0:
+				exit(1);
+			case 1:
+				printf("\nEnter the data that needs to be inserted in a struct tree\n");
+				scanf("%d", &data);
+				insert(&root_node, data);
+				break;
+			case 2:
+				data = delete(&root_node);
+				printf("\nThe deleted data is: %d\n", data);
+				break;
+			case 3:
+				inorder(root_node);
+				break;
+			case 4:
+				preorder(root_node);
+				break;
+			case 5:
+				postorder(root_node);
+				break;
+			case 6:
+				height = find_height(root_node);
+				printf("\nThe height of a tree is: %d\n", height);
+				break;
+			case 7:
+				height = find_height(root_node);
+				print_level_order(root_node, height);
+				break;
+			default:
+				break;
+		}
+	}
+
+	return 0;
+}
+
+static void insert(struct tree **root_node, int data)
+{
+	if (*root_node == NULL)
+	{
+		*root_node = (struct tree *)malloc(sizeof(struct tree *));
+		(*root_node)->data = data;
+		(*root_node)->left_child = NULL;
+		(*root_node)->right_child = NULL;
+	}
+	else
+	{
+		if (data < (*root_node)->data)
+		{
+			insert(&((*root_node)->left_child), data);
+		}
+		else if (data > (*root_node)->data)
+		{
+			insert(&((*root_node)->right_child), data);
+		}
+	}
+}
+
+static int delete(struct tree **root_node)
+{
+	/* Do nothing. */
+}
+
+static int inorder(struct tree *root_node)
+{
+	if (root_node != NULL)
+	{
+		inorder(root_node->left_child);
+		printf("\n%d\n", root_node->data);
+		inorder(root_node->right_child);
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+static int preorder(struct tree *root_node)
+{
+	if (root_node != NULL)
+	{
+		printf("\n%d\n", root_node->data);
+		preorder(root_node->left_child);
+		preorder(root_node->right_child);
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+static int postorder(struct tree *root_node)
+{
+	if (root_node != NULL)
+	{
+		postorder(root_node->left_child);
+		postorder(root_node->right_child);
+		printf("\n%d\n", root_node->data);
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+static int find_height(struct tree *root_node)
+{
+	int left_height = 0;
+	int right_height = 0;
+
+	if (root_node == NULL)
+	{
+		return -1;
+	}
+	else
+	{
+		left_height = find_height(root_node->left_child);
+		right_height = find_height(root_node->right_child);
+		return (max(left_height, right_height) + 1);
+	}
+}
+
+static int max(int left_height, int right_height)
+{
+	return ((left_height > right_height) ? left_height : right_height);
+}
+
+static void print_level_order(struct tree *root_node, int height)
+{
+	int index = 0;
+
+	for (index = 1; index <= (height + 1); ++index)
+	{
+		print_data(root_node, index);
+	}
+}
+
+static int print_data(struct tree *root_node, int level)
+{
+	if (root_node == NULL)
+		return 0;
+	if (level == 1)
+	{
+		printf("\n%d\n", root_node->data);
+	}
+	else if (level > 1)
+	{
+		print_data(root_node->left_child, level - 1);
+		print_data(root_node->right_child, level - 1);
+	}
+}
